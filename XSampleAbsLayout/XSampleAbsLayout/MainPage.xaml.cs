6a4c041e5ad6a5ec86c4e467404ab5303df60d14
hirotaka <musicfair@gmail.com>
2017-07-29 10:47:11 +0900

    画像変更/画像のタップ対応

--- XSampleAbsLayout/XSampleAbsLayout/MainPage.xaml.cs
+++ XSampleAbsLayout/XSampleAbsLayout/MainPage.xaml.cs
@@ -12,6 +12,23 @@ namespace XSampleAbsLayout
         public MainPage()
         {
             InitializeComponent( );
+
+            Image_user.Source = ImageSource.FromResource( "XSampleAbsLayout.Resources.Images.image_user.png" );
+            Image_frame.Source = ImageSource.FromResource( "XSampleAbsLayout.Resources.Images.image_frame.png" );
+
+            var tgr_user = new TapGestureRecognizer( );
+            tgr_user.Tapped += ( s, e ) =>
+            {
+                DisplayAlertDialog( );
+            };
+            Image_user.GestureRecognizers.Add( tgr_user );
+            
+        }
+
+        private void DisplayAlertDialog( )
+        {
+            DisplayAlert( "", "Tap", "OK" );
         }
     }
+
 }
